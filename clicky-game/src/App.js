38761b2a731862ecfff0311e5084001caa1275d5
Loren <lorenrose@icloud.com>
2017-12-10 22:31:00 -0600

    images

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Images from "./components/Images";
+import photo from "./pictures.json";
+import shuffle from 'shuffle-array';
+
+shuffle(photo);
+
+let currentScore = 0;
+
+let highScore = 0;
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  state = {
+    photo
+  };
+
+  setSelected = (id) => {
+    console.log(id);
+    this.setState({ selected: true});
+  }
+
+render () {
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="text-center">Clicky Game</h1>
+        <h2 className="text-right">Your Score: {currentScore} </h2>
+        <h2 className="text-right">High Score: {highScore} </h2>
       </div>
-    );
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+              {this.state.photo.map(photos => {
+                return (
+                  <Images
+                    key={photos.id}
+                    name={photos.name}
+                    id={photos.id}
+                    image={photos.image}
+                    selected={photos.selected}
+                    setSelected={this.setSelected}
+                  />
+                )
+              })}
+          </div>
+        </div>
+
+    </div>
+  );
   }
 }
+
 
 export default App;
